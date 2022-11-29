edd3ca1886c938daac26ced15b1091f82c1b2bc2
ujjawal sharma <112757910+Sharmaujjawal123@users.noreply.github.com>
2022-11-29 20:33:07 +0530

    Update ATM PROJECT.py

--- ATM PROJECT.py
+++ ATM PROJECT.py
@@ -59,20 +59,20 @@ Please enter the four digit PIN: """))
             print("YOUR CURRENT BALANCE IS: ",balance)
         if choice==2:
             deposite=int(input("PLEASE ENTER AMOUNT TO DEPOSITE: "))
-            balance1=balance+deposite
-            print(f"YOUR CURRENT BALANCE IS: {balance1}")
+            balance=balance+deposite
+            print(f"YOUR CURRENT BALANCE IS: {balance}")
         if choice==3:
             withdraw=int(input("PLEASE ENTER AMOUNT TO WITHDRAW: "))
             if withdraw>balance:
                 print("INSUFFICIENT BALANCE!!")
             else:    
-                balance2=balance-withdraw
-                if balance2<=2000:
-                    print(f"YOUR CURRENT BALANCE IS: {balance2}")
+                balance=balance-withdraw
+                if balance<=2000:
+                    print(f"YOUR CURRENT BALANCE IS: {balance}")
                     print(f"YOUR ACCOUNT HAVE LOW BALANCE PLEASE MAKE SUFFICIENT BALANCE GREATER THAN 2000")
 
                 else:
-                    print(f"YOUR CURRENT BALANCE IS: {balance2}")
+                    print(f"YOUR CURRENT BALANCE IS: {balance}")
         if choice==4:
             print("""
             
